565eb4715e4c741878d101a2543edd4a649213b8
tsion-hailu <hailutsion16@gmail.com>
2024-10-19 15:44:45 +0300

    updates

--- src/API/axiosConfig.jsx
+++ src/API/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosBase = axios.create({
   // baseURL: "http://localhost:5500/api",
-  baseURL: "https://new-evan-7.onrender.com"
+  baseURL: "https://new-evan-7.onrender.com/api"
 });
 
 export default axiosBase;
